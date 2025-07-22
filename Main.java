a0604bae9da9b8059c6681c6bb3c1a28b8d34c87
RavSinghChandan <ravchandan15@gmail.com>
2025-07-22 16:24:27 +0530

    done

--- Main.java
+++ Main.java
@@ -6,18 +6,26 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "java developer full stack";
+        String s1 = "listen";
+        String s2 = "silent";
 
-        f(s);
+        f(s1,s2);
     }
-    private static void f(String s){
 
-       String s1 =  Arrays.stream(s.split(" "))
-                      .map(word -> new StringBuilder(word).reverse().toString())
-               .collect(Collectors.joining(" "));
-        System.out.println(s1);
+    private static void f(String s1,String s2){
 
+        Map<Character,Integer> chotu1 = new HashMap<>();
+        Map<Character,Integer> chotu2 = new HashMap<>();
 
+        for(char c1 : s1.toCharArray()){
+            chotu1.put(c1,chotu1.getOrDefault(c1,0)+1);
+        }
+        for(char c2 : s2.toCharArray()){
+            chotu2.put(c2,chotu2.getOrDefault(c2,0)+1);
+        }
+        System.out.println(chotu1);
+        System.out.println(chotu2);
+        System.out.println(chotu1.equals(chotu2));
 
     }
 

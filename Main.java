eb28537c499c3e2d59ec0560dd21aad7f56ddc88
RavSinghChandan <ravchandan15@gmail.com>
2025-07-22 05:44:23 +0530

    done

--- Main.java
+++ Main.java
@@ -1,20 +1,24 @@
-import java.util.Arrays;
-import java.util.OptionalInt;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-       int[] a = {1,2,3,4};
+        String s = "java developer full stack";
 
-        System.out.println( max(a));
+        f(s);
+    }
+    private static void f(String s){
+
+       String s1 =  Arrays.stream(s.split(" "))
+                      .map(word -> new StringBuilder(word).reverse().toString())
+               .collect(Collectors.joining(" "));
+        System.out.println(s1);
+
+
 
     }
-    private  static OptionalInt max(int[] a ){
-       return  Arrays.stream(a).max();
 
-    }
-    private static  void printArray(int [] a) {
-        for (int e : a) {
-            System.out.print(e + " ");
-        }
-    }
 }
